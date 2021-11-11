8bf1c44781284698eb5ae1f9e27e593057ad2054
Denys Tarasiuk <tarasiuk.denys@gmail.com>
2021-11-12 01:14:41 +0200

    Add to HomeTask

---
+++ src/ua/hillel/denys/hometask/family/FamilyMain.java
@@ -0,0 +1,14 @@
+package ua.hillel.denys.hometask.family;
+
+public class FamilyMain {
+
+    public static void main(String[] args) {
+
+        Family family1 = new Family("Папа", "Виктор");
+        Family family2 = new Family("Мама", "Рая");
+        Family family3 = new Family("Сестра", "Вика");
+        Family family4 = new Family("Я", "Денис");
+
+    }
+
+}
